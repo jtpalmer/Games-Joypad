ec69a5b72aa5f86d064c51c6944d36d5e5e41117
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2012-06-18 21:01:00 -0400

    Initial commit

---
+++ lib/Games/Joypad.pm
@@ -0,0 +1,40 @@
+package Games::Joypad;
+use strict;
+use warnings;
+
+# ABSTRACT: Undocumented module
+
+1;
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Games::Joypad;
+
+=head1 DESCRIPTION
+
+What does this module do?
+
+=head1 METHODS
+
+=head2 method
+
+=head1 CAVEATS
+
+=head1 BUGS
+
+=head1 RESTRICTIONS
+
+=head1 NOTES
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<perl>
+
+=back
+
+=cut
+
---
+++ t/00-load.t
@@ -0,0 +1,23 @@
+#!perl
+use strict;
+use warnings;
+use Test::More;
+
+BEGIN {
+    my @modules = qw(
+        Games::Joypad
+    );
+
+    for my $module (@modules) {
+        use_ok($module) or BAIL_OUT("Failed to load $module");
+    }
+}
+
+diag(
+    sprintf(
+        'Testing Games::Joypad %f, Perl %f, %s', $Games::Joypad::VERSION, $], $^X
+    )
+);
+
+done_testing();
+
